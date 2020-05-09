5bd9234008a516ae7748b55c09ee13f7e1051ae4
jack <jackmead515@gmail.com>
2020-05-08 22:19:55 -1000

    changing ip

--- config/default.js
+++ config/default.js
@@ -3,6 +3,6 @@ module.exports = {
   UPDATE_HOST: '0.0.0.0',
   RELAY_PORT: 5656,
   RELAY_HOST: '0.0.0.0',
-  PANTRY_HOST: '0.0.0.0:5757',
+  PANTRY_HOST: '192.168.0.6:5757',
   SQL_PATH: '.auto_farm/database.db',
 }
